bb08cea9267ee58702467f211ef5402a9c130aeb
profe Joel <joel.torres@ulagos.cl>
2020-10-01 16:59:38 -0300

    Clase 6 - Completa programa laberinto

--- UNIDAD_II/laberinto.py
+++ UNIDAD_II/laberinto.py
@@ -1,4 +1,4 @@
-from colections import deque
+from collections import deque
 import time
 import sys, os
 
@@ -35,14 +35,15 @@ class nodo_estado:
 
     def get_accion(self):
         return self.accion
+
+    def get_nivel(self):
+        return self.nivel
     
     def __eq__(self, e):
         return self.valor == e
     
 class laberinto:
-    estados_finales = []
-    for final in estados_finales:
-        estados_finales.append(nodo_estado(final, None, "Final", None))
+    estados_finales = [nodo_estado([3,4], None, "Final", None),nodo_estado([1,4], None, "Final", None)]
 
     def __init__(self, EI, mapa):
         self.estado_inicial = nodo_estado(EI, None, "Origen", 1)
@@ -57,7 +58,7 @@ class laberinto:
         self.historial.append(e)
 
     def pop(self):
-        self.cola_estados.popleft()
+        return self.cola_estados.popleft()
 
     def esta_en_historial(self, e):
         return e in self.historial
@@ -67,7 +68,7 @@ class laberinto:
 
     def mostrar_estado_actual(self):
         os.system('cls')
-        print("Estado Actual es: \n")
+        print("Estado Actual es: [" + str(self.estado_actual.get_estado()[0]) +","+ str(self.estado_actual.get_estado()[1]) + "]\n")
         x = self.estado_actual.get_estado()
         print(x)
         ce = self.cola_estados
@@ -94,6 +95,44 @@ class laberinto:
                 j += 1
             print(texto)
             i += 1
+
+    def mostrar_solucion(self, e):
+        os.system('cls')
+        x = self.estado_actual.get_estado()
+        print(x)
+        h = self.historial
+        ce = self.cola_estados
+        sol = self.solucion
+        lab = self.laberinto
+        texto = ""
+        i = 0
+
+        for fila in lab:
+            texto = ""
+            j = 0
+            for lugar in fila:
+                a = [i,j]
+                if lugar == "1":
+                    letra = "#"
+                elif x[0] == i and x[1] == j:
+                    letra = "O"
+                elif a in self.estados_finales:
+                    letra = "F"
+                elif a in h:
+                    if a in ce:
+                        letra = "-"
+                    elif a in sol:
+                        letra = "*"
+                    else:
+                        letra = "h"
+                else:
+                    letra = " "
+                
+                texto += letra
+                j += 1
+            print(texto)
+            i += 1
+
 
     def buscar_padres(self, e):
         if e.get_padre() == None:
@@ -137,8 +176,94 @@ class laberinto:
                 nueva_coordenada[0] = fila
                 nueva_coordenada[1] = columna + 1
 
-        if laberinto[nueva_coordenada[0],nueva_coordenada[1]] == "1":
+        if laberinto[nueva_coordenada[0]][nueva_coordenada[1]] == "1":
             return "illegal"
         else:
             return nueva_coordenada
                 
+    
+    def algoritmo_anchura(self, EI):
+        iteracion = 1
+        self.estado_actual = EI
+        movimientos = ["N","S","O","E"]
+
+        while(not self.es_final()):
+            print("Iteracion: " + str(iteracion) + "\n")
+            self.mostrar_estado_actual()
+
+            for movimiento in movimientos:
+                estado_temporal = nodo_estado(self.mover(movimiento), self.estado_actual, "Mover a " + movimiento, self.estado_actual.get_nivel() + 1)
+                if not self.esta_en_historial(estado_temporal) and not estado_temporal.get_estado() == "illegal":
+                    self.add(estado_temporal) # se incluye en historial y en la cola
+
+            print("\nElementos en Historial: " + str(len(self.historial)))
+            print("\nElementos en Cola Estados: " + str(len(self.cola_estados)))
+
+            #Paso a siguiente iteracion
+            self.estado_actual = self.pop()
+            iteracion += 1
+            time.sleep(1)
+        
+        print("Iteracion: " + str(iteracion) + "\n")
+        self.mostrar_estado_actual()
+        print("\n\n\nHa llegado a Solucion!!!")
+        self.buscar_padres(self.estado_actual)
+        self.mostrar_solucion(self.estado_inicial)
+        print("\nALGORITMO EN ANCHURA:")
+        print("\nElementos en Historial: " + str(len(self.historial)))
+        print("\nElementos en Cola Estados: " + str(len(self.cola_estados)))
+        print("\nCantidad de Iteraciones: " + str(iteracion))
+
+    def add_profundidad(self, pila_sucesores):
+        while pila_sucesores.__len__() > 0:
+            e = pila_sucesores.pop()
+            self.historial.append(e)
+            self.cola_estados.appendleft(e)
+
+    def algoritmo_profundidad(self, EI):
+        iteracion = 1
+        self.estado_actual = EI
+        movimientos = ["N", "S", "O", "E"]
+        sucesores = deque()
+
+        while not self.es_final():
+            print("Iteracion: " + str(iteracion) + "\n")
+            self.mostrar_estado_actual()
+
+            for movimiento in movimientos:
+                estado_temporal = nodo_estado(self.mover(movimiento), self.estado_actual, "Mover a " + movimiento, self.estado_actual.get_nivel() + 1)
+                if not self.esta_en_historial(estado_temporal) and not estado_temporal.get_estado() == "illegal":
+                    sucesores.append(estado_temporal)
+            
+            self.add_profundidad(sucesores) 
+
+            print("\nElementos en Historial: " + str(len(self.historial)))
+            print("\nElementos en Cola Estados: " + str(len(self.cola_estados)))
+
+            #Paso a siguiente iteracion
+            self.estado_actual = self.pop()
+            iteracion += 1
+            time.sleep(1)
+
+        print("Iteracion: " + str(iteracion) + "\n")
+        self.mostrar_estado_actual()
+        print("\n\n\nHa llegado a Solucion!!!")
+        self.buscar_padres(self.estado_actual)
+        self.mostrar_solucion(self.estado_inicial)
+        print("\nALGORITMO EN PROFUNDIDAD:")
+        print("\nElementos en Historial: " + str(len(self.historial)))
+        print("\nElementos en Cola Estados: " + str(len(self.cola_estados)))
+        print("\nCantidad de Iteraciones: " + str(iteracion))
+
+
+    def busqueda(self):
+        self.add(self.estado_inicial)
+        #self.algoritmo_anchura(self.pop())
+        self.algoritmo_profundidad(self.pop())
+
+
+
+if __name__ == "__main__":
+    coordenada = [0,0]
+    lab = laberinto(coordenada, leer_mapa("laberinto_simple.dat"))
+    lab.busqueda()
